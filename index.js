ed4b25ae87c987f666c3ad38aa24c2d3d6a6c2b3
Linh <linh44h@gmail.com>
2018-09-16 01:17:14 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file
+function findMatching(arr, name) {
+  return arr.filter( function (name1) {
+    return name1.toLowerCase() === name.toLowerCase();
+  })
+}
+
+function fuzzyMatch(arr, name) {
+  // return arr.filter ( function (name1) {
+  //   return name1.charAt(1) === name.charAt(1);
+  // })
+  let len = name.length;
+  return arr.filter ( function (n) {
+    debugger;
+    return n.slice(0, len) === name;
+  })
+}
+
+function matchName(arr, name) {
+  return arr.filter(function (n) {
+    return n.name === name;
+  })
+}
