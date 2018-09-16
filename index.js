a1f0770c57027bce47d4131ac8b6c2b341849e87
github username <github email address>
2018-09-16 11:40:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, char) {
+  return drivers.filter(d => d[0] === char[0])
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(obj => 
+     obj["name"] === name
+  )
+}
+
+// function deepIterator (target) {
+//   if (typeof target === 'object') {
+//     for (const key in target) {
+//       deepIterator(target[key]);
+//     }
+//   } else {
+//     console.log(target);
+//   }
+// }
