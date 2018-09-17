8e86169e4bdf0ad5ca9ee6f2bd8ce07c66e0f624
haleybarlar <hbarlar1@gmail.com>
2018-09-17 14:38:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(function (driver) {
+    return driver.startsWith(str);
+  });
+}
+
+
+// Ruby Equivalent
+// drivers.select do |driver|
+//   driver.startsWith(str)
+// end
+
+function matchName(drivers, str) {
+  return drivers.filter(function (driver){
+    return driver.name === str;
+  });
+}
